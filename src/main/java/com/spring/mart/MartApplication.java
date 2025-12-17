5b14c35dc1b89eada0cbc8842085b14cabab06f8
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2025-12-17 23:18:05 +0530

    Add: docker setup and health endpoint (#1)

--- src/main/java/com/spring/mart/MartApplication.java
+++ src/main/java/com/spring/mart/MartApplication.java
@@ -2,12 +2,13 @@ package com.spring.mart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MartApplication.class, args);
     }
-
 }
